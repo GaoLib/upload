88738ae85c8dee9bb3bf26e88b35c94d6419952c
Gao <xingaoemail@163.com>
2021-09-16 14:06:22 +0800

    feat: add login page

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,6 +14,7 @@ module.exports = {
   // add your custom rules here
   rules: {
     'nuxt/no-cjs-in-config': 'off',
-    "vue/comment-directive": 'off'
+    'vue/comment-directive': 'off',
+    'space-before-function-paren': 0
   }
 }
