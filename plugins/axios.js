06c6d771268570dc4491a0dec7b7f547525f2103
Gao <xingaoemail@163.com>
2021-10-03 12:42:23 +0800

    feat：axios 处理登录过期

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,28 +1,43 @@
 import Vue from 'vue'
 import axios from 'axios'
+import { MessageBox } from 'element-ui'
 
 const service = axios.create({
   baseURL: '/api'
 })
 
-// 请求拦截
-service.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem('token')
-    if (token) {
-      config.headers.common.Authorization = 'Bearer ' + token
+export default ({ store, redirect }) => {
+  // 请求拦截
+  service.interceptors.request.use(
+    (config) => {
+      const token = localStorage.getItem('token')
+      if (token) {
+        config.headers.common.Authorization = 'Bearer ' + token
+      }
+      return config
     }
-    return config
-  }
-)
+  )
 
-// 响应拦截
-service.interceptors.response.use(
-  (response) => {
-    const { data } = response
-    return data
-  }
-)
+  // 响应拦截
+  service.interceptors.response.use(
+    (response) => {
+      const { data } = response
+
+      if (data.code === -666) {
+        MessageBox.confirm('登录已过期', '过期', {
+          confirmButtonText: '登录',
+          showCancelButton: false,
+          type: 'warning'
+        }).then(() => {
+          localStorage.removeItem('token')
+          redirect({ path: 'login' })
+        })
+      }
+
+      return data
+    }
+  )
+}
 
 Vue.prototype.$http = service
 
