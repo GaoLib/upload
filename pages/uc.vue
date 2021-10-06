5a966e3ace6d0524d92c4eb2c53f00cca8f34fbe
Gao <xingaoemail@163.com>
2021-10-06 16:14:47 +0800

    feat: 推拽上传及进度条

--- pages/uc.vue
+++ pages/uc.vue
@@ -1,8 +1,11 @@
 <template>
   <div>
     <h1>用户中心</h1>
-    <div>
+    <div ref="drag" class="drag">
       <input @change="handleFileChanged" type="file" name="file"></input>
+    </div>
+    <div class="progress">
+      <el-progress :stroke-width="20" :text-inside="true" :percentage="uploadProgress" />
     </div>
     <div>
       <el-button @click="uploadFile">
@@ -16,13 +19,32 @@
 export default {
   data() {
     return {
-      file: null
+      file: null,
+      uploadProgress: 0
     }
   },
   async mounted() {
     await this.$http.get('/user/info')
+    this.bindEvents()
   },
   methods: {
+    bindEvents() {
+      const drag = this.$refs.drag
+      drag.addEventListener('dragover', (e) => {
+        drag.style.borderColor = 'red'
+        e.preventDefault()
+      })
+      drag.addEventListener('dragleave', (e) => {
+        drag.style.borderColor = '#eee'
+        e.preventDefault()
+      })
+      drag.addEventListener('drop', (e) => {
+        const fileList = e.dataTransfer.files
+        drag.style.borderColor = '#eee'
+        this.file = fileList[0]
+        e.preventDefault()
+      })
+    },
     handleFileChanged(e) {
       const file = e.target.files[0]
       if (file) {
@@ -33,12 +55,23 @@ export default {
       const form = new FormData()
       form.append('name', 'file')
       form.append('file', this.file)
-      await this.$http.post('uploadfile', form)
+      await this.$http.post('uploadfile', form, {
+        onUploadProgress: (progress) => {
+          this.uploadProgress = Number((progress.loaded / progress.total) * 100).toFixed(2)
+        }
+      })
     }
   }
 }
 </script>
 
-<style lang="scss" scoped>
-
+<style lang="stylus" scoped>
+.drag
+  width 200px
+  height 100px
+  border 2px dashed #eee
+  text-align center
+  line-height 100px
+.progress
+  width 200px
 </style>
